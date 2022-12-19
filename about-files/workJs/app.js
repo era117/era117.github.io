253174e067411a47c6913861e2b72357e35a9118
saya-loo <200103459@stu.sdu.edu.kz>
2022-12-19 20:32:18 +0600

    dada

---
+++ about-files/workJs/app.js
@@ -0,0 +1,8 @@
+document.getElementById('btn').onclick = function() {
+=  let main = document.body;
+    if (main.classList.contains('dark')) {
+      main.classList.remove('dark');
+    } else{
+      main.classList.add('dark');
+    }
+}
